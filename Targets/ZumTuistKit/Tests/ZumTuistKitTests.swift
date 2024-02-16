ecea5e8f0120e56f09d6946afc8822050c25bf37
J0onYEong <cjy2134@naver.com>
2024-02-16 13:46:15 +0900

    [Feat] tuist init적용

---
+++ Targets/ZumTuistKit/Tests/ZumTuistKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class ZumTuistKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("ZumTuistKit", "ZumTuistKit")
+    }
+}
---
+++ Targets/ZumTuistUI/Tests/ZumTuistUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class ZumTuistUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("ZumTuistUI", "ZumTuistUI")
+    }
+}
