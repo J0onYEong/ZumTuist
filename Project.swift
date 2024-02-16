1af187ee6a22930aff35c7e1b354fc0985d28cb3
J0onYEong <cjy2134@naver.com>
2024-02-16 13:47:41 +0900

    [Refactor] 프로젝트 클리어

--- Project.swift
+++ Project.swift
@@ -1,29 +1,3 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 import MyPlugin
-
-/*
-                +-------------+
-                |             |
-                |     App     | Contains ZumTuist App target and ZumTuist unit-test target
-                |             |
-         +------+-------------+-------+
-         |         depends on         |
-         |                            |
- +----v-----+                   +-----v-----+
- |          |                   |           |
- |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
- |          |                   |           |
- +----------+                   +-----------+
-
- */
-
-// MARK: - Project
-
-// Local plugin loaded
-let localHelper = LocalHelper(name: "MyPlugin")
-
-// Creates our project using a helper function defined in ProjectDescriptionHelpers
-let project = Project.app(name: "ZumTuist",
-                          platform: .iOS,
-                          additionalTargets: ["ZumTuistKit", "ZumTuistUI"])
--- Targets/ZumTuistKit/Tests/ZumTuistKitTests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class ZumTuistKitTests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("ZumTuistKit", "ZumTuistKit")
-    }
-}
