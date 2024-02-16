a53a92f8952dbc473ab50c6a933d075bf5fc4971
J0onYEong <cjy2134@naver.com>
2024-02-16 14:01:33 +0900

    [Feat] Target.create함수 생성

--- Project.swift
+++ Project.swift
@@ -2,18 +2,3 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 import MyPlugin
 
-extension Project {
-    
-    public func create(name: String, packages: [Package], settings: Settings, target: [Target], schemes: [Scheme]) -> Project {
-        
-        return Project(
-            name: name,
-            organizationName: "Junios Adventure",
-            packages: packages,
-            settings: settings,
-            schemes: schemes
-        )
-        
-    }
-
-}
--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,1 +1,40 @@
 import ProjectDescription
+
+extension Project {
+    
+    public func create(name: String, packages: [Package], settings: Settings, target: [Target], schemes: [Scheme]) -> Project {
+        
+        return Project(
+            name: name,
+            organizationName: "Junios Adventure",
+            packages: packages,
+            settings: settings,
+            schemes: schemes
+        )
+        
+    }
+
+}
+
+extension Target {
+    
+    static private var defaultBundleId = "com.junios"
+    
+    private func create(name: String, product: Product, infoPlist: InfoPlist, isNeedResource: Bool, dependencies: [TargetDependency]) -> Target {
+        
+        let resources: ResourceFileElements = ["Resources/**"]
+        
+        return Target(
+            name: name,
+            platform: .iOS,
+            product: product,
+            bundleId: "\(Self.defaultBundleId).\(name)",
+            infoPlist: infoPlist,
+            sources: [
+                "Sources/**"
+            ],
+            resources: isNeedResource ? resources : [],
+            dependencies: dependencies
+        )
+    }
+}
