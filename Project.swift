432858550f5daecb64cd260a54bdc034539a893f
J0onYEong <cjy2134@naver.com>
2024-02-16 13:53:58 +0900

    [Feat] Project extension생성

--- Project.swift
+++ Project.swift
@@ -1,3 +1,19 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 import MyPlugin
+
+extension Project {
+    
+    public func create(name: String, packages: [Package], settings: Settings, target: [Target], schemes: [Scheme]) -> Project {
+        
+        return Project(
+            name: name,
+            organizationName: "Junios Adventure",
+            packages: packages,
+            settings: settings,
+            schemes: schemes
+        )
+        
+    }
+
+}
